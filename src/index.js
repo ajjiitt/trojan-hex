b619184fef66f0e170d0589eacdb0dcd725c44b0
ajit <ajitdakre05@gmail.com>
2022-03-28 13:34:26 +0530

    Updated CSS

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-
+import "./main.css"
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
